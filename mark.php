9b7f9c537ee1c6c25ec90baa924a32845213b244
Ross ifill <rossifill@hotmail.co.uk>
2018-05-30 17:07:55 +0100

    Update mark.php

--- mark.php
+++ mark.php
@@ -1,3 +1,26 @@
 <?php
 
 require_once 'includes/dbh.inc.php';
+
+if (isset($_GET['as'], $_GET['item'])) {
+	$as = $_GET['as'];
+	$item = $_GET['item'];
+
+	switch ($as) {
+		case 'done':
+			$doneQuery = $db->prepare("
+				UPDATE items
+				SET done = 1
+				WHERE id = :item
+				AND users = :users
+				");
+
+				$doneQuery->execute([
+					'item' => $item,
+					'users' => $_SESSION['users_id']
+				]);
+			break;
+	}
+}
+
+header('location: index.php');
